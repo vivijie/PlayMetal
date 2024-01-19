3b0589cf2872ce0e931b4fbdf5e9e96d0ebd2095
Neil <j.uloveit@gmail.com>
2024-01-19 16:22:34 +0800

    Initial Commit

---
+++ MetalPlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalPlay
+//
+//  Created by NEIL WANG on 2024/1/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalPlay/MetalPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalPlayApp.swift
+//  MetalPlay
+//
+//  Created by NEIL WANG on 2024/1/19.
+//
+
+import SwiftUI
+
+@main
+struct MetalPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
